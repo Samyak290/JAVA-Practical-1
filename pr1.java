89f236bb6567bd3b23e6c52ed140595d69f00913
Samyak Ramteke <samyakrmk@gmail.com>
2026-01-29 19:33:19 +0530

    Create pr1.java

---
+++ pr1.java
@@ -0,0 +1,19 @@
+public class pr1 {
+    public static void main(String[] args) {
+        byte b = 67;
+        short s = 7777;
+        long l = 345676656;
+        int i = 58;
+        double d = 890556.3;
+        String str = "Samyak";
+        float f=7.6f;
+        System.out.println("Byte: "+b);
+        System.out.println("Short: "+s);
+        System.out.println("Long: "+l);
+        System.out.println("INTEGER: "+i);
+        System.out.println("Double: "+d);
+        System.out.println("String: "+str);
+        System.out.println("Float: "+f);
+
+    }
+}
